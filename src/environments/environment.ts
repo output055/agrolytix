0ca2528d1aab2eddc86f82ab851ddd1447839c6e
output055 <asamoahclement055@gmail.com>
2026-06-08 17:48:31 +0000

    scroll fxn

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://agrolytixapi.asatek.online/api',
+  // apiUrl: 'https://agrolytixapi.asatek.online/api',
+  apiUrl: 'http://127.0.0.1:8000/api',
 };
